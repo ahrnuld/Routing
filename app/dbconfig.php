ad28f1a6a9ed75d874c1656356789e86e5e84380
Mark <mark.dehaan@inholland.nl>
2021-12-17 15:12:04 +0100

    changed db to postgres

--- app/dbconfig.php
+++ app/dbconfig.php
@@ -1,6 +1,12 @@
 <?php
-$servername = "mysql";
-$username = "root";
-$password = "secret123";
-$database = "developmentdb";
+$type = "pgsql";
+$servername = "ec2-52-49-23-139.eu-west-1.compute.amazonaws.com";
+$username = "sisxbrgyysdwds";
+$password = "8145ac6d4baaadb0f916efe885157f2a15cf8fabc087e235a2cf0fba0c822e24";
+$database = "dcnvtrr7n5cbv";
+$port = 5432;
+// $servername = "mysql";
+// $username = "root";
+// $password = "secret123";
+// $database = "developmentdb";
 ?>

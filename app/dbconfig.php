eefffe59fc0a223ca4185c5d0ca615c17dc72d9b
Mark <mark.dehaan@inholland.nl>
2021-12-17 15:38:21 +0100

    added db setup

--- app/dbconfig.php
+++ app/dbconfig.php
@@ -1,10 +1,11 @@
 <?php
+
 $type = "pgsql";
 $servername = "ec2-52-49-23-139.eu-west-1.compute.amazonaws.com";
 $username = "sisxbrgyysdwds";
 $password = "8145ac6d4baaadb0f916efe885157f2a15cf8fabc087e235a2cf0fba0c822e24";
 $database = "dcnvtrr7n5cbv";
-$port = 5432;
+//$type = "mysql";
 // $servername = "mysql";
 // $username = "root";
 // $password = "secret123";
